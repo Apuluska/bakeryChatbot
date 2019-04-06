c610cac438004859aa678f8715fb952fb2620a02
Paula G. Rodrigo <paulagarciarodrigo@MacBook-Pro-de-Paula.local>
2019-04-06 17:35:08 +0200

    api connected, predefined messages

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+var express = require('express');
+var app = express();
+var rp = require('request-promise');
 
-app.get('/', (req, res) => {
-    res.send('Hola2');
+var textList = ['hola', 'que hay de menu', 'adios'];
+var answerList = ['Buenos dias, bienvenido a BakeryChatbot', 'nuestro menu del día es:', 'hasta otra, gracias por venir'];
+
+function getMenu() {
+    rp('https://xz94zfs6u8.execute-api.eu-west-1.amazonaws.com/default/myBakery')
+        .then(function (vienenTodosLosdatos) {
+            console.log(vienenTodosLosdatos);
+            /*  var datosParseados = JSON.parse(vienenTodosLosdatos);
+             var monedaExtranjera = datosParseados.rates[monedaDelUsuario].toString();
+             res.send(monedaExtranjera); */
+        }).catch((error) => { console.log('error') })
+}
+
+function getResponse(key) {
+    let response = '';
+    switch (key) {
+        case String.contains('hola'):
+            response = answerList[0];
+            break;
+        case String.contains('menu'):
+            response = answerList[1] + getMenu();
+            break;
+        case String.contains('adios'):
+            response = answerList[2];
+            break;
+        default:
+            response = 'While you will be attendat, take a look to our menu' + getMenu();
+            break;
+    }
+    return response;
+}
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
-//estableciendo puerto de escucha
-app.listen(port, () => {
-    console.log(`Estoy escuchando en el puerto ${port}`);
-});
+
