04efd63b3e4798ae9ea93f11e613527de2121d71
Paula G. Rodrigo <paulagarciarodrigo@MacBook-Pro-de-Paula.local>
2019-04-07 14:04:38 +0200

    chat working just in node

--- index.js
+++ index.js
@@ -2,39 +2,43 @@ var express = require('express');
 var app = express();
 var rp = require('request-promise');
 
-var textList = ['hola', 'que hay de menu', 'adios'];
-var answerList = ['Buenos dias, bienvenido a BakeryChatbot', 'nuestro menu del día es:', 'hasta otra, gracias por venir'];
+var textList = ['hola', 'menu', 'adios'];
+var answerList = ['Buenos dias, bienvenido a BakeryChatbot', 'nuestro menu del día es: ', 'hasta otra, gracias por venir '];
 
-function getMenu() {
-    rp('https://xz94zfs6u8.execute-api.eu-west-1.amazonaws.com/default/myBakery')
-        .then(function (vienenTodosLosdatos) {
-            console.log(vienenTodosLosdatos);
-            /*  var datosParseados = JSON.parse(vienenTodosLosdatos);
-             var monedaExtranjera = datosParseados.rates[monedaDelUsuario].toString();
-             res.send(monedaExtranjera); */
-        }).catch((error) => { console.log('error') })
+
+app.get("/", (req, res)=> {
+   console.log("Responding to root route")
+   res.send("Hello from Root")
+})
+
+
+/// SERVER IS LISTENING
+app.listen(3000, ()=> {
+    console.log("Server is ok")
+})
+async function getMenu() {
+    var a = await rp('https://xz94zfs6u8.execute-api.eu-west-1.amazonaws.com/default/myBakery')
+    .then(function(htmlString) {
+        return htmlString;
+    });  
+    return a;
 }
 
-function getResponse(key) {
-    let response = '';
-    switch (key) {
-        case String.contains('hola'):
-            response = answerList[0];
-            break;
-        case String.contains('menu'):
-            response = answerList[1] + getMenu();
-            break;
-        case String.contains('adios'):
-            response = answerList[2];
-            break;
-        default:
-            response = 'While you will be attendat, take a look to our menu' + getMenu();
-            break;
+async function getResponse(key) {
+    if (key === textList[0]) {
+        return answerList[0];   
     }
-    return response;
-}
+    if (key.includes(textList[1])) {
+        var a = answerList[1]+ await getMenu();
+        return a;   
+    } else {
+        return answerList[2];
+    }
+ }
+ async function initChatbot(){
+     var op= await getResponse('quiero saber el menu del dia');
+     console.log(op);
+ }
+initChatbot();
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
-
+ 
